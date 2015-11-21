95cfa9475f47a94af219bc02c2b94a4b6a32a2dd
g0tmi1k <have.you.g0tmi1k@gmail.com>
2015-11-21 09:26:31 +0000

    Made EXEC high level blind

--- vulnerabilities/exec/source/high.php
+++ vulnerabilities/exec/source/high.php
@@ -19,6 +19,9 @@ if( isset( $_POST[ 'Submit' ]  ) ) {
 
 	// Remove any of the charactars in the array (blacklist).
 	$target = str_replace( array_keys( $substitutions ), $substitutions, $target );
+	
+	// Default response
+	$result = "Host down";
 
 	// Determine OS and execute the ping command.
 	if( stristr( php_uname( 's' ), 'Windows NT' ) ) {
@@ -30,8 +33,15 @@ if( isset( $_POST[ 'Submit' ]  ) ) {
 		$cmd = shell_exec( 'ping  -c 4 ' . $target );
 	}
 
+	// Was the host up?
+	// Linux: 64 bytes from 127.0.0.1: icmp_req=1 ttl=64 time=0.023 ms
+	// Windows: Reply from ::1: time<1ms
+	if ( preg_match ( '*from*', $cmd ) ) {
+		$result = "Host up";
+	}
+	
 	// Feedback for the end user
-	$html .= "<pre>{$cmd}</pre>";
+	$html .= "<pre>{$result}</pre>";
 }
 
 ?>
